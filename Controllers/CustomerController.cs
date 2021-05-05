c0ffd3a483c7ebe7b93bf362f03685a9729ece5c
joao2391 <j_paulo_15@hotmail.com>
2021-05-05 07:47:06 -0300

    remove using

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,5 +1,4 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LiteDB_Example.Services;
 using LiteDB_Example.Models;
